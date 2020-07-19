b371506912624bf7d9778f13265f9456b3d5c138
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2020-07-19 09:55:55 -0700

    Demo app

---
+++ demo/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SlateRTE from "slate-rte";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SlateRTE />
+      </div>
+    );
+  }
+}
+
+export default App;
