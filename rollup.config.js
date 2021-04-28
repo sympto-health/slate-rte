a2a569d70e6f90b5a50c2345e732218d832fb5d5
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2021-04-28 15:54:10 -0700

    Reduce package size

--- rollup.config.js
+++ rollup.config.js
@@ -10,16 +10,10 @@ export default {
   input: "src/index.tsx",
   output: [
     {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true
-    },
-    {
       file: pkg.module,
       format: "es",
       exports: "named",
-      sourcemap: true
+      sourcemap: false
     }
   ],
   plugins: [
