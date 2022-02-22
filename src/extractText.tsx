b3d0a382caed00335ff834b3a4fa90f8463d3648
Prithvi Narasimhan <narasimhan.prithvi@gmail.com>
2022-02-21 22:46:01 -0800

    Option to include new lines whne extracting text

--- src/extractText.tsx
+++ src/extractText.tsx
@@ -3,11 +3,14 @@ import { SlateNode } from './SlateNode'
 
 // iterates through every single slate item and children and stringifies everything by extracting
 // all teh text
-const extractText = (slateContent: null | SlateNode[], variables: { [variableName: string]: string }): string => (_.compact(_.flatten((slateContent || [])
+const extractText = (
+  slateContent: null | SlateNode[], variables: { [variableName: string]: string }, includeNewLines: undefined | boolean = false,
+): string => (_.compact(_.flatten((slateContent || [])
   .map((contentItem: SlateNode) => ([
+  	(includeNewLines && contentItem.type != null ? '\n' : ''),
     (contentItem.text ? String(contentItem.text) : ' '),
     (contentItem.type === 'variable' ? variables[contentItem.variableName] : ''),
-    ...(contentItem.children ? extractText((contentItem.children as null | SlateNode[]), variables) : []),
+    ...(contentItem.children ? extractText((contentItem.children as null | SlateNode[]), variables, includeNewLines) : []),
   ]))))).join('');
 
 
